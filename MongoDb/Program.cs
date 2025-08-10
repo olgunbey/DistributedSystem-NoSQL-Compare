2afd931b0a4600592e6fed5918557892813e7e60
olgunbey <olgunsahin0161@hotmail.com>
2025-08-10 19:25:07 +0300

    Add project files.

---
+++ MongoDb/Program.cs
@@ -0,0 +1,82 @@
+﻿
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System.Text.Json;
+
+static async Task AddToCollection()
+{
+
+    var connectionString = "mongodb://127.0.0.1:27017,127.0.0.1:27018,127.0.0.1:27019/,127.0.0.1:27020/?replicaSet=rs0";
+
+    using (MongoClient client = new MongoClient(connectionString))
+    {
+        var database = client.GetDatabase("test");
+
+        var person = new Person
+        {
+            Name = "Olgunbey Şahin",
+            Age = 24
+        };
+        var options = new MongoCollectionSettings
+        {
+            WriteConcern = new WriteConcern(WriteConcern.WMode.Majority)
+        };
+        var collection = database.GetCollection<BsonDocument>("testCollection", options);
+        var bsonDocument = BsonDocument.Parse(JsonSerializer.Serialize(person));
+        await collection.InsertOneAsync(bsonDocument);
+    }
+}
+
+static async Task<List<BsonDocument>> ReadCollection()
+{
+    var connectionString = "mongodb://127.0.0.1:27017,127.0.0.1:27018,127.0.0.1:27019/,127.0.0.1:27020/?replicaSet=rs0";
+
+    using (MongoClient client = new MongoClient(connectionString))
+    {
+        var database = client.GetDatabase("test");
+
+        var person = new Person
+        {
+            Name = "John Doe",
+            Age = 30
+        };
+        var options = new MongoCollectionSettings
+        {
+            ReadConcern = new ReadConcern(ReadConcernLevel.Available)
+        };
+        try
+        {
+            var collection = database.GetCollection<BsonDocument>("testCollection", options);
+            var bsonDocument = BsonDocument.Parse(JsonSerializer.Serialize(person));
+            var data =(await collection.FindAsync(FilterDefinition<BsonDocument>.Empty)).ToList();
+            return data;
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+       
+
+        
+    }
+}
+
+Console.WriteLine("Hello, World!");
+
+await AddToCollection();
+//await ReadCollection();
+
+Console.ReadLine();
+
+
+
+public class Person
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
